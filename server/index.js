eb92bfda8e90cb481f1b53a4bc53d19f16d2da23
Nolan Vanmoortel <nolan@o2b.dev>
2020-04-16 01:03:30 +0200

    fix issue port env heroku

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
-/* eslint-disable */
+// server.js
 const express = require('express');
+const compression = require('compression');
 const path = require('path');
 const app = express();
 
+app.use(compression());
 app.use(express.static(path.join(__dirname, '../build')));
 
-app.get('/*', function(req, res) {
+app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
-app.listen(8080);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}`);
+});
